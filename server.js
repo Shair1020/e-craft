a710f91e45cbe1777bb79b6b31bb031b6b3fa7f1
shair1020 <ali99shair@gmail.com>
2021-06-01 19:47:41 +0500

    mongodb atlas connected

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const mongoDB_password = "ebWVEBSSr4FfWza5";
+mongoose
+  .connect("mongodb+srv://shairali:ebWVEBSSr4FfWza5@cluster0.ihaqw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("Connected to mongodb");
+    console.log(con.connections);
+  });
 const app = express();
 
 app.listen(8000, () => {
-  console.log("Server running on 8000");    
+  console.log("Server running on 8000");
 });
