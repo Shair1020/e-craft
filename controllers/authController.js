8e9e40eb3df98aaef95a6f47e4235aa974cb826d
shair1020 <ali99shair@gmail.com>
2021-08-30 23:38:15 +0500

    done with sign up api and fecth users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,36 @@
 const User = require("../models/authModel")
+const JWT = require("jsonwebtoken");
+
+
+exports.fetchUsers = async (req, res) => {
+    try {
+        const user = await User.find();
+        res.status(200).json({
+            status: "Sucess",
+            data: {
+                user,
+            }
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: "error",
+            error: error.message
+        })
+
+    }
+}
 
 exports.signup = async (req, res) => {
     try {
         const user = await User.create(req.body);
+        const { password, ...modifiedUser } = user.toObject()
+        ///JWT TOKEN
+        const token = JWT.sign({ id: user._id }, process.env.JWT_WEB_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
         res.status(200).json({
-            status: "Sucees",
+            status: "Sucess",
+            token,
             data: {
-                user,
+                user: modifiedUser,
             }
         })
     } catch (error) {
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const { signup } = require("../controllers/authController");
+const { signup, fetchUsers } = require("../controllers/authController");
 const route = express.Router();
-route.post("/signup", signup)
+
+route.get("/", fetchUsers);
+route.post("/signup", signup);
 
 module.exports = route;
