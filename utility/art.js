1dabd2477cc7f49e159b920cba3cd762230f453d
shair1020 <ali99shair@gmail.com>
2021-12-26 01:28:04 +0500

    create itility function in art js

--- utility/art.js
+++ utility/art.js
@@ -1,10 +1,8 @@
-exports.shapeArtData = (req) => {
-    const {body, files, user} = req;
+exports.shapeArtData = (body, files) => {
     const gallery = [];
-    files.forEach(({filename}) => {
+    files.forEach(({ filename }) => {
         gallery.push(filename)
     })
     body.gallery = gallery;
-    body.artist = user._id
     return body;
 }
